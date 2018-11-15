de1648aca561b2ee7f047d86de341a56d2358876
JerrallK <JerrallK@gmail.com>
2018-11-15 17:42:32 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TensorFlow__study
+####主要参考书籍：《TensorFlow：实战Google深度学习框架》
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import tensorflow as tf
+from numpy.random import RandomState
 
 
 def test_1():
@@ -43,6 +45,42 @@ def test_4():
         #需要初始化！
         sess.run(w1.initializer)
         print("sdf",w1.eval())
+
+def test5():
+    batch_size=8
+    w1=tf.Variable(tf.random_normal((2,3),stddev=1,seed=1),name="w1")
+    w2 = tf.Variable(tf.random_normal((3, 1), stddev=1, seed=1),name='w2')
+    # 定义placeholder
+    # 作为存放输入数据的地方。这里维度也不一定要定义。
+    # 但如果维度是确定的，那么给出维度可以降低出错的概率。
+    #x=tf.constant([[0.7,0.9]],name='x')
+
+    x=tf.placeholder(tf.float32,shape=(None,2), name="x-input")
+    y_=tf.placeholder(tf.float32,shape=(None,2), name="y-input")
+
+    a=tf.matmul(x,w1,name='a')
+    y=tf.matmul(a,w2,name='y')
+
+    with tf.Session() as sess:
+        init_op=tf.global_variables_initializer()
+        sess.run(init_op)
+        print(sess.run(y,feed_dict={x:[[0.7,0.9],[0.2,0.6],[0.9,0.5]]}))
+        writer=tf.summary.FileWriter("F:\PycharmProjects\Tensorflow\log666" ,tf.get_default_graph())
+        writer.close()
+
+
+        y=tf.sigmoid(y)
+        cross_entropy=-tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y,1e-10,1.0))
+                                      +(1-y)*tf.log(tf.clip_by_value(-y,1e-10,1.0)))
+
+        learnint_rate=0.001
+        train_step=tf.train.AdamOptimizer(learnint_rate).minimize(cross_entropy)
+
+        rdm=RandomState()
+        dataset_size=128
+
+        X=rdm.rand(dataset_size,2)
+        Y = [[int(xl + x2 < 1)] for (xl, x2) in X]
 if __name__ == "__main__":
     #test_1()
     #test_2()
