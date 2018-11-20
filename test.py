9c1f8f35fa9c98311d3495be0745de82a470c7db
JerrallK <JerrallK@gmail.com>
2018-11-20 16:14:11 +0800

    test_8

--- test.py
+++ test.py
@@ -141,6 +141,53 @@ def test_7():
             x_value=sess.run(x)
             print ("After %s iteration(s): x%s is %f."% (i+1, i+1, x_value) )
 
+def get_weight(shape,lambda_):
+    #获取－层神经网络边上的权茧，并将这个权重的L2正则化损失加入名称为’ losses ’的集合中
+
+    var=tf.Variable(tf.random.normal(shape),dtype=tf.float32)
+
+    # add to collection 函数将这个新生成变量的L2 正则化损失项加入集合。
+    #这个函数的第一个参数’ losses ’ 是集合的名字，第二个参数是要加入这个集合的内容。
+    tf.add_to_collection("losses" ,tf.contrib.layers.l2_regularizer(lambda_)(var))
+    return var
+
+
+def test_8():
+    x=tf.placeholder(tf.float32,shape=(None,2))
+    y=tf.placeholder(tf.float32,shape=(None,2))
+
+    batch_size=8
+    # 定义了每一层的节点数
+    layer_dimension=[2,10,10,10,1]
+    #神经网络的层数
+    n_layers=len(layer_dimension)
+    #这个变量维护前向传播时最深层的节点，开始的时候就是输入层。
+    cur_layer=x
+    #当前层的节点个数。
+    in_dimension=layer_dimension[0]
+    #通过一个循环来生成5层全连接的冲经网络结构。
+    for i in range(1,n_layers):
+        out_dimension=layer_dimension[i]
+        #某一层的参数
+        weight=get_weight([in_dimension,out_dimension])
+        bias=tf.Variable(tf.constant(0.1,shape=[out_dimension]))
+        #RELU激活函数
+        cur_layer=tf.nn.relu(tf.matmul(cur_layer,weight)+bias)
+        #更新下一层的节点个数
+        in_dimension=layer_dimension[i]
+
+    #在定义神经网络前向传播的同时已经将所有的L2正则化损失加入了图上的集合，
+    #这里只需要计算刻画模型在训练数据上表现的损失函数。
+    #此时cur_layer已经是最后一层
+    mse_loss=tf.reduce_mean(tf.square(y_-cur_layer))
+
+    #将均方误差损失函数加入损失集合。
+    tf.add_to_collection('losses' ,mes_loss)
+
+    # get_collection返回一个列表，这个列表是所有这个集合中的元素。在这个样例中
+    #这些元素就是损失函数的不同部分，将它们加起来就可以得到最终的损失函数。
+
+    loss=tf.add_n(tf.get_collection('losses'))
 
 
 if __name__ == "__main__":
